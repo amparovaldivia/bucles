a04fb57a89c438f1bdd9aebf8d25d9183b3df7f0
unknown <amparo.valdivia@gmail.com>
2022-08-27 03:50:08 -0400

    first commit

---
+++ bucles.js
@@ -0,0 +1,25 @@
+for (var x = 1; x <= 20; x++) {
+    if (x % 2 == 1) {
+        console.log(x);
+    }
+}
+for (var x=100;x>=0;x--){
+    if(x%3==0){
+        console.log(x);
+    }
+}
+var y=[4,2.5,1,-0.5,-2,-3.5];
+for (var x=0;x<y.length;x++){
+    console.log(y[x]);
+}
+var sum=0;
+for(var x=0;x<=100;x++){
+    sum=sum+x;
+}
+console.log(sum);
+
+var por=1;
+for( var x=1;x<=12;x++){
+    por=por*x;
+}
+console.log(por);
